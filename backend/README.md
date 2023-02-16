17ef9a5d6424e91efd104300e7da4c60df990632
hachiyuki8 <caladuil@outlook.com>
2023-02-15 21:57:48 -0500

    set up basic tests

--- backend/README.md
+++ backend/README.md
@@ -15,3 +15,7 @@ Runs the JavaScript generated with npm build, for the production environment.
 ### npm run dev
 
 Runs the dev server in watch mode, so any saved changes should be picked up automatically and the server restarted. Use this for development. You also shouldn't have to 'npm run build' at all when developing, this script automatically complies the TypeScript code in watch mode for you and runs the dev server.
+
+### npm test
+
+Runs the test suite.
--- backend/tests/models/customer.ts
+++ backend/tests/models/customer.ts
@@ -3,19 +3,25 @@ import { expect } from 'chai';
 
 describe('Testing making new customer', function () {
   it('1. Invalid if username empty', function (done) {
-    const c = new Customer({ email: "abc@gmail.com" })
-    c.validate(function (err) {
-      expect(err.errors.username).to.exist
-      done()
-    })
+    var c = new Customer({ email: "abc@gmail.com" })
+    c.validate(function(err) {
+      if (err) {
+        // @ts-ignore 
+        expect(err.errors.username).to.exist; 
+        done();
+      }
+    });
   })
 
   it('2. Invalid if email empty', function (done) {
     const c = new Customer({ username: "admin" })
-    c.validate(function (err) {
-      expect(err.errors.email).to.exist
-      done()
-    })
+    c.validate(function(err) {
+      if (err) {
+        // @ts-ignore 
+        expect(err.errors.email).to.exist;
+        done();
+      }
+    });
   })
 
   it('3. Creating new customer', function (done) {
