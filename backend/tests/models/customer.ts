b42019128194608d1a9c24f6b199086f4029a584
hachiyuki8 <caladuil@outlook.com>
2023-02-15 21:39:51 -0500

    set up mongoose

---
+++ backend/tests/models/customer.ts
@@ -0,0 +1,29 @@
+import Customer from '../../models/customer'
+import { expect } from 'chai';
+
+describe('Testing making new customer', function () {
+  it('1. Invalid if username empty', function (done) {
+    const c = new Customer({ email: "abc@gmail.com" })
+    c.validate(function (err) {
+      expect(err.errors.username).to.exist
+      done()
+    })
+  })
+
+  it('2. Invalid if email empty', function (done) {
+    const c = new Customer({ username: "admin" })
+    c.validate(function (err) {
+      expect(err.errors.email).to.exist
+      done()
+    })
+  })
+
+  it('3. Creating new customer', function (done) {
+    const c = new Customer({ username: "admin", email: "abc@gmail.com" })
+    c.validate(function () {
+      expect(c.username).to.exist
+      expect(c.email).to.exist
+      done()
+    })
+  })
+})
