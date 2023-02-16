d00945380b2ee8a83f0b9ee9868373a4190b2439
Roshni_Surpur <rsurpur20@choate.edu>
2023-02-16 16:43:08 -0500

    added donut and customer page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import EmployeePage from './pages/EmployeePage';
+import CustomerPage from './pages/CustomerPage';
+import DonutPage from './pages/DonutPage';
 import OrderPage from './pages/OrderPage';
 import Layout from './components/Layout';
 
@@ -13,6 +15,8 @@ function App (props : AppProps) {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<HomePage />} />
                     <Route path="employee" element={<EmployeePage />} />
+                    <Route path="customer" element={<CustomerPage />} />
+                    <Route path="donut" element={<DonutPage />} />
                     <Route path="order" element={<OrderPage />} />
                 </Route>
             </Routes>
