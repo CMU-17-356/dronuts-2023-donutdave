46129cf9cc09c415c76f7fb17bab890d88b0c271
Steven Wu <37231181+stevenwu2019@users.noreply.github.com>
2023-02-28 00:20:47 -0500

    Implemented basic checkout page (#36)
    * Created checkout forms
    
    * Edited form fields and theming
    
    * Added basic snapshot tests

---
+++ frontend/src/components/Review.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Grid from '@mui/material/Grid';
+
+const products = [
+  {
+    name: 'Strawberry Frosted',
+    desc: '1 dozen',
+    price: '$9.99',
+  },
+  {
+    name: 'Chocolate Glaze',
+    desc: '1/2 dozen',
+    price: '$5.65',
+  },
+  {
+    name: 'Boston Kreme',
+    desc: '1 pc.',
+    price: '$1.00',
+  },
+  { name: 'Shipping', desc: '', price: 'Free' },
+];
+const addresses = ['5522 Kentucky Ave', 'Pittsburgh', 'PA', '15232', 'USA'];
+const payments = [
+  { name: 'Card type', detail: 'Visa' },
+  { name: 'Card holder', detail: 'Mr Steven Wu' },
+  { name: 'Card number', detail: 'xxxx-xxxx-xxxx-1234' },
+  { name: 'Expiry date', detail: '04/2024' },
+];
+
+export default function Review() {
+  return (
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        Order summary
+      </Typography>
+      <List disablePadding>
+        {products.map((product) => (
+          <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
+            <ListItemText primary={product.name} secondary={product.desc} />
+            <Typography variant="body2">{product.price}</Typography>
+          </ListItem>
+        ))}
+        <ListItem sx={{ py: 1, px: 0 }}>
+          <ListItemText primary="Total" />
+          <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
+            $16.64
+          </Typography>
+        </ListItem>
+      </List>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+            Shipping
+          </Typography>
+          <Typography gutterBottom>Steven Wu</Typography>
+          <Typography gutterBottom>{addresses.join(', ')}</Typography>
+        </Grid>
+        <Grid item container direction="column" xs={12} sm={6}>
+          <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+            Payment details
+          </Typography>
+          <Grid container>
+            {payments.map((payment) => (
+              <React.Fragment key={payment.name}>
+                <Grid item xs={6}>
+                  <Typography gutterBottom>{payment.name}</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography gutterBottom>{payment.detail}</Typography>
+                </Grid>
+              </React.Fragment>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
+}
