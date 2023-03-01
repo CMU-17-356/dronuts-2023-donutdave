017b4f14f737bac23c76ce7e2b172d8b03607ee3
Philip Kaufholz <pkaufhol@andrew.cmu.edu>
2023-02-28 20:11:40 -0500

    remove unused imports

--- frontend/src/pages/CustomerPage.tsx
+++ frontend/src/pages/CustomerPage.tsx
@@ -1,29 +1,14 @@
 import { Link } from 'react-router-dom';
-import * as React from 'react';
 import Button from '@mui/material/Button';
-
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-
-
-// import Link from '@mui/material/Link';
-
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import DonutCard from '../components/DonutCard';
 
 type CustomerPageProps = {}
-  
-
-
 
 const theme = createTheme();
-
 
 function CustomerPage (props : CustomerPageProps) {
 
