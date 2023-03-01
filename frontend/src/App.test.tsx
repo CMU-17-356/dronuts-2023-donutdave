a3fd54a0754153af33436adc829a3531a5c7db0e
Philip Kaufholz <pkaufhol@andrew.cmu.edu>
2023-02-28 20:08:40 -0500

    remove obselete test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,14 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
-import App from './App';
+//import App from './App';
 import CheckoutPage from './pages/CheckoutPage';
 import SignUpPage from './pages/SignUpPage';
 
-test('full app rendering', async () => {
-    render(<App />)
-    // verify page content for default route
-    expect(screen.getByText(/This is the home page./i)).toBeInTheDocument()
-})
+// test('full app rendering', async () => {
+//     render(<App />)
+//     // verify page content for default route
+//     expect(screen.getByText(/This is the home page./i)).toBeInTheDocument()
+// })
   
 test('signup page rendering/navigating', async () => {
   render(<SignUpPage />)
