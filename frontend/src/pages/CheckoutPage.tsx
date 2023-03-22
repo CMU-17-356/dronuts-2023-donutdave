d18df6affcf4d9fa4e4b49b09cdf0e63582c7e93
Philip Kaufholz <pkaufhol@andrew.cmu.edu>
2023-03-21 21:48:33 -0400

    fix linter errors

--- frontend/src/pages/CheckoutPage.tsx
+++ frontend/src/pages/CheckoutPage.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import Paper from '@mui/material/Paper';
